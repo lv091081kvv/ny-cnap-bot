7d7b2f4d58b0b63dc7dbf1e99e1007d43335450c
Vasyl Karakevych <vasyl.karakevych@gmail.com>
2022-06-17 14:48:28 +0300

    add function start

--- ny-cnap-bot.py
+++ ny-cnap-bot.py
@@ -1,1 +1,12 @@
-#chatbot for cnap of Novoiavorivsk city
+# chatbot for cnap of Novoiavorivsk city
+import telebot
+
+bot = telebot.TeleBot('5524104596:AAGYVLYuPzAlBICN40434FPsuxbsNXbqq8g')
+
+@bot.message_handler(commands=['start'])
+def start(message):
+    bot.send_message(message.chat.id, "Вас вітає ЦНАП м.Новояворівськ", parse_mode='html')
+
+
+
+bot.polling(none_stop=True)
