86f19bdf10e0e8a7174f7423ecfb33a2cf9be779
Vasyl Karakevych <vasyl.karakevych@gmail.com>
2022-06-20 11:32:56 +0300

    add link to website

--- ny-cnap-bot.py
+++ ny-cnap-bot.py
@@ -1,5 +1,6 @@
 # chatbot for cnap of Novoiavorivsk city
 import telebot
+from telebot import types
 
 bot = telebot.TeleBot('5524104596:AAGYVLYuPzAlBICN40434FPsuxbsNXbqq8g')
 
@@ -16,18 +17,18 @@ def start(message):
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
 # send user text
-@bot.message_handler(content_types=['text'])
-def get_user_text(message):
-    if message.text == "id":
-        mess = f"Your ID= {message.from_user.id}"
-    elif message.text == "photo":
-        photo = open('smile.jpg', 'rb')
-        bot.send_photo(message.chat.id, photo)
-        bot.send_photo(message.chat.id, "FILEID")
-    else:
-        mess = "Вибачте. Я вас не розумію."
-        
-    bot.send_message(message.chat.id, mess, parse_mode='html')
+# @bot.message_handler(content_types=['text'])
+# def get_user_text(message):
+#     if message.text == "id":
+#         mess = f"Your ID= {message.from_user.id}"
+#     elif message.text == "photo":
+#         photo = open('smile.jpg', 'rb')
+#         bot.send_photo(message.chat.id, photo)
+#         bot.send_photo(message.chat.id, "FILEID")
+    # else:
+    #     mess = "Вибачте. Я вас не розумію."
+    #
+    # bot.send_message(message.chat.id, mess, parse_mode='html')
 
 # send user photo
 @bot.message_handler(content_types=['photo'])
@@ -35,9 +36,15 @@ def get_user_text(message):
     bot.send_message(message.chat.id, 'Гарне фото', parse_mode='html')
 
 # create link to site
+@bot.message_handler(commands=['website'])
+def get_user_text(message):
+    markup = types.InlineKeyboardMarkup()
+    markup.add(types.InlineKeyboardButton('Перейти на сайт', url="https://novoyavmiskrada.org.ua"))
+    bot.send_message(message.chat.id, '&#8595 Натисніть на ссилку для переходу на офіційний сайт &#8595'
+                     , parse_mode='html', reply_markup=markup)
 
 
-
+# create buttons
 
 
 bot.polling(none_stop=True)
