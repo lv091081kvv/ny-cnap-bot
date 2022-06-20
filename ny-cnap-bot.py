2d99d8c58d8817e2a437fa62495fca1412f4c0c9
Vasyl Karakevych <vasyl.karakevych@gmail.com>
2022-06-20 10:09:10 +0300

    add finish

--- ny-cnap-bot.py
+++ ny-cnap-bot.py
@@ -3,12 +3,20 @@ import telebot
 
 bot = telebot.TeleBot('5524104596:AAGYVLYuPzAlBICN40434FPsuxbsNXbqq8g')
 
+# send user command start
 @bot.message_handler(commands=['start'])
 def start(message):
     mess = f"Добрий день <b>{message.from_user.first_name} {message.from_user.last_name}</b>\nВас вітає ЦНАП Новояворівської міської ради."
     bot.send_message(message.chat.id, mess, parse_mode='html')
 
-@bot.message_handler()
+# send user command finish
+@bot.message_handler(commands=['finish'])
+def start(message):
+    mess = f"Дякую <b>{message.from_user.first_name} {message.from_user.last_name}</b> що нас відвідали.\nГарного дня."
+    bot.send_message(message.chat.id, mess, parse_mode='html')
+
+# send user text
+@bot.message_handler(content_types=['text'])
 def get_user_text(message):
     if message.text == "id":
         mess = f"Your ID= {message.from_user.id}"
@@ -20,6 +28,16 @@ def get_user_text(message):
         mess = "Вибачте. Я вас не розумію."
         
     bot.send_message(message.chat.id, mess, parse_mode='html')
+
+# send user photo
+@bot.message_handler(content_types=['photo'])
+def get_user_text(message):
+    bot.send_message(message.chat.id, 'Гарне фото', parse_mode='html')
+
+# create link to site
+
+
+
 
 
 bot.polling(none_stop=True)
